2dd7f016fd6fb942fdf2104e5f618d928e4ad971
Anant <anantsinghraghuvanshi1@gmail.com>
2023-08-17 20:16:03 +0530

    methods added in service

--- hub-backend/src/main/java/com/CodeCraft/hubbackend/service/TaskService.java
+++ hub-backend/src/main/java/com/CodeCraft/hubbackend/service/TaskService.java
@@ -1,12 +1,12 @@
 package com.CodeCraft.hubbackend.service;
 
 import com.CodeCraft.hubbackend.model.Task;
-import com.google.cloud.firestore.CollectionReference;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.*;
 import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 @Service
@@ -20,6 +20,24 @@ public class TaskService {
         task.setId(addedDocRef.getId());
         return task;
     }
-
-
+    public List<Task> getAllTasks() throws ExecutionException, InterruptedException {
+        List<QueryDocumentSnapshot> documents = dbFirestore.collection("tasks").get().get().getDocuments();
+        List<Task> tasks = new ArrayList<>()
+        for (DocumentSnapshot document : documents) {
+            tasks.add(document.toObject(Task.class));
+        }
+        return tasks;
+    }
+    public Task getTaskById(String id) throws ExecutionException, InterruptedException{
+        DocumentSnapshot document = dbFirestore.collection("tasks").document(id).get().get();
+        return document.toObject(Task.class);
+    }
+    public String updateTask(String id, Task task){
+        dbFirestore.collection("tasks").document(id).set(task);
+        return "Task updated successfully";
+    }
+    public String deleteTask(String id){
+        dbFirestore.collection("tasks").document(id).delete();
+        return "Task deleted successfully";
+    }
 }
