d8f741be2d5e49ad52b344e1164656da4a42bc81
Anant <anantsinghraghuvanshi1@gmail.com>
2023-08-18 13:25:38 +0530

    task service created

--- hub-frontend/src/app/tasks/task-list/task-list.component.ts
+++ hub-frontend/src/app/tasks/task-list/task-list.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TaskService } from 'src/app/services/taskService/task.service';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.css']
+  styleUrls: ['./task-list.component.css'],
 })
 export class TaskListComponent {
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.taskService.getAllTasks().subscribe((data: Task[]) => {
+      this.tasks = data;
+    });
+  }
+
+  onTaskSelect(taskId: string): void {
+    this.router.navigate(['/tasks', taskId]);
+  }
 }
