27062998ab7565acc1a56dccb1e46aad25f96493
Anant <anantsinghraghuvanshi1@gmail.com>
2023-08-18 13:09:13 +0530

    routing configured for tasks

--- hub-frontend/src/app/app-routing.module.ts
+++ hub-frontend/src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+import { TaskCreateComponent } from './tasks/task-create/task-create.component';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
   },
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'tasks/:id', component: TaskDetailComponent },
+  { path: 'tasks/create', component: TaskCreateComponent },
 ];
 
 @NgModule({
