ef5450e16dd29db86d1bb4ecc4758dfd9a993ed7
Anant <anantsinghraghuvanshi1@gmail.com>
2023-08-19 18:54:11 +0530

    auth service added

--- hub-frontend/src/app/registration/registration.component.ts
+++ hub-frontend/src/app/registration/registration.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+  styleUrls: ['./registration.component.css'],
 })
 export class RegistrationComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      this.authService
+        .register(this.registerForm.value)
+        .subscribe(/* handle response */);
+    }
+  }
 }
