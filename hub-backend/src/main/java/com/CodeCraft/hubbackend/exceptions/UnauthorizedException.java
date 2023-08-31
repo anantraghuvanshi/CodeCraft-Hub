2bfe97c1fc05870ce7d97b8fac85df627a185397
Anant <anantsinghraghuvanshi1@gmail.com>
2023-09-01 01:19:50 +0530

    UnauthorizedException added

---
+++ hub-backend/src/main/java/com/CodeCraft/hubbackend/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.CodeCraft.hubbackend.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
