7389b299f77be765096a9d045fa37468257e5675
Anant <anantsinghraghuvanshi1@gmail.com>
2023-09-03 01:30:00 +0530

    file update

--- hub-backend/src/main/java/com/CodeCraft/hubbackend/configuration/SecurityConfig.java
+++ hub-backend/src/main/java/com/CodeCraft/hubbackend/configuration/SecurityConfig.java
@@ -3,43 +3,32 @@ package com.CodeCraft.hubbackend.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
-@EnableMethodSecurity
-public class SecurityConfig  {
+public class SecurityConfig {
+
 
     @Autowired
-    private UserDetailsService userDetailsService;
-
+    private JwtAuthenticationEntryPoint point;
     @Autowired
-    private JwtRequestFilter jwtRequestFilter;
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-    }
+    private JwtAuthenticationFilter filter;
 
     @Bean
-    public HttpSecurity securityFilterChain(HttpSecurity http) throws Exception{
-        http
-                .cors().and()
-                .csrf().disable()
-                .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/api/auth/**").permitAll()
-                        .anyRequest().authenticated()
-                );
-        return http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-    }
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())
+                .authorizeRequests().
+                requestMatchers("/auth/login").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().exceptionHandling(ex -> ex.authenticationEntryPoint(point))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 
 
