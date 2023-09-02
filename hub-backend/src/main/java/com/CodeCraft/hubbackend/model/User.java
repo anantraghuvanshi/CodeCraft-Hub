4ba4cfad39e9d559f1e6e03fa4e27aa7dbddc962
Anant <anantsinghraghuvanshi1@gmail.com>
2023-09-03 01:03:14 +0530

    user.java update for jwt

--- hub-backend/src/main/java/com/CodeCraft/hubbackend/model/User.java
+++ hub-backend/src/main/java/com/CodeCraft/hubbackend/model/User.java
@@ -1,11 +1,18 @@
 package com.CodeCraft.hubbackend.model;
 
-public class User {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class User implements UserDetails {
 
     private String id;
     private String userName;
     private String email;
     private String password;
+
+
 
     public String getId() {
         return id;
@@ -31,8 +38,38 @@ public class User {
         this.email = email;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 
     public void setPassword(String password) {
