3ff47f3b0eb530f61a5f41aa92e489d9c5ea6e75
Anant <anantsinghraghuvanshi1@gmail.com>
2023-09-03 13:13:01 +0530

    AuthenticationService configured

--- hub-backend/src/main/java/com/CodeCraft/hubbackend/controller/AuthenticationService.java
+++ hub-backend/src/main/java/com/CodeCraft/hubbackend/controller/AuthenticationService.java
@@ -1,10 +1,15 @@
 package com.CodeCraft.hubbackend.controller;
 
-import org.springframework.security.core.userdetails.User;
+import com.CodeCraft.hubbackend.configuration.JwtService;
+import com.CodeCraft.hubbackend.model.User;
 import com.CodeCraft.hubbackend.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
 
 @Service
 @RequiredArgsConstructor
@@ -12,16 +17,35 @@ public class AuthenticationService {
 
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
-
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+        userService.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
                 .build();
 
     }
 
-    public AuthenticationResponse login(AuthenticationRequest request) {
+    public AuthenticationResponse login(AuthenticationRequest request) throws ExecutionException, InterruptedException {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = userService.findByUsernameRepo(request.getEmail());
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
