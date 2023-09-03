43e4ea11f6d619e1614905e0d6d4d54fa9853e78
Anant <anantsinghraghuvanshi1@gmail.com>
2023-09-04 00:17:40 +0530

    User.java update

--- hub-backend/src/main/java/com/CodeCraft/hubbackend/model/User.java
+++ hub-backend/src/main/java/com/CodeCraft/hubbackend/model/User.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 
 @Data
 @Builder
@@ -23,7 +24,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Collections.emptyList();
     }
 
     @Override
