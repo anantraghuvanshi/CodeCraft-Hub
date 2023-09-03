1e9afd44f1cae16c8e1458a49bbd4c0e55134bff
Anant <anantsinghraghuvanshi1@gmail.com>
2023-09-03 13:11:44 +0530

    requestMatchers added

--- hub-backend/src/main/java/com/CodeCraft/hubbackend/configuration/SecurityConfig.java
+++ hub-backend/src/main/java/com/CodeCraft/hubbackend/configuration/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
 
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests().
-                requestMatchers("/auth/login")
+                requestMatchers("/api/v1/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
